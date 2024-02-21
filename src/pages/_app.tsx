1e49d3d8bc8aca20f10c4517b4484d8a99ac8a15
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-21 21:14:00 +0000

    Improve head metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+import Head from "next/head";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
+      <Head>
+        <title>Duty to do</title>
+        <meta name="description" content="App to help you choose what to do" />
+        <link rel="icon" href="/test.svg" />
+      </Head>
       <Component {...pageProps} />;
     </ClerkProvider>
   );
