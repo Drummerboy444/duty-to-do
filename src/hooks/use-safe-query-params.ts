d4c94e08766c73d7858749bb6e8d5042aca05276
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-21 21:09:16 +0000

    Bring over some useful utils

---
+++ src/hooks/use-safe-query-params.ts
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+
+const getSafeQueryParamsHook =
+  <T extends string>(keys: T[]) =>
+  () => {
+    const { query, isReady } = useRouter();
+
+    if (!isReady) return "LOADING";
+
+    if (keys.some((key) => typeof query[key] !== "string"))
+      return "QUERY_PARAMS_UNAVAILABLE";
+
+    return keys.reduce(
+      (previousLookup, key) => ({
+        ...previousLookup,
+        [key]: query[key],
+      }),
+      {} as Record<T, string>,
+    );
+  };
