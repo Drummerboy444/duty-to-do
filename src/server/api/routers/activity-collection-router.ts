91d546b06f7186282b143917daed43b9f8b94c0a
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-24 23:17:34 +0000

    Add activity collection CRUD operations to API

--- src/server/api/routers/activity-collection-router.ts
+++ src/server/api/routers/activity-collection-router.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure } from "../trpc";
 import { ACCESS_DENIED, SUCCESS } from "../utils/generic-responses";
+import { isUniqueConstraintViolation } from "../utils/db-violations";
 
 export const activityCollectionRouter = createTRPCRouter({
   get: privateProcedure
@@ -31,4 +32,106 @@ export const activityCollectionRouter = createTRPCRouter({
       }),
     };
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx: { db, userId }, input: { name, description } }) => {
+      const preprocessedName = name.trim();
+      if (preprocessedName === "") return { type: "EMPTY_NAME" as const };
+
+      const preprocessedDescription = description.trim();
+      if (preprocessedDescription === "")
+        return { type: "EMPTY_DESCRIPTION" as const };
+
+      try {
+        return {
+          type: "SUCCESS" as const,
+          activityCollection: await db.activityCollection.create({
+            data: {
+              ownerId: userId,
+              name: preprocessedName,
+              description: preprocessedDescription,
+            },
+          }),
+        };
+      } catch (error) {
+        if (isUniqueConstraintViolation(error)) {
+          return { type: "ACTIVITY_COLLECTION_ALREADY_EXISTS" as const };
+        }
+
+        throw error;
+      }
+    }),
+
+  edit: privateProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        description: z.string(),
+      }),
+    )
+    .mutation(
+      async ({ ctx: { db, userId }, input: { id, name, description } }) => {
+        const preprocessedName = name.trim();
+        if (preprocessedName === "") return { type: "EMPTY_NAME" as const };
+
+        const preprocessedDescription = description.trim();
+        if (preprocessedDescription === "")
+          return { type: "EMPTY_DESCRIPTION" as const };
+
+        const activityCollection = await db.activityCollection.findUnique({
+          where: { id },
+        });
+
+        if (activityCollection === null) {
+          return { type: "NO_ACTIVITY_COLLECTION_FOUND" as const };
+        }
+
+        const canEditActivityCollection = activityCollection.ownerId === userId;
+
+        if (!canEditActivityCollection)
+          return { type: "ACCESS_DENIED" as const };
+
+        return {
+          type: "SUCCESS" as const,
+          activityCollection: await db.activityCollection.update({
+            where: { id },
+            data: {
+              name: preprocessedName,
+              description: preprocessedDescription,
+            },
+          }),
+        };
+      },
+    ),
+
+  delete: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx: { db, userId }, input: { id } }) => {
+      const activityCollection = await db.activityCollection.findUnique({
+        where: { id },
+      });
+
+      if (activityCollection === null) {
+        return { type: "NO_ACTIVITY_COLLECTION_FOUND" as const };
+      }
+
+      const canDeleteActivityCollection = activityCollection.ownerId === userId;
+
+      if (!canDeleteActivityCollection)
+        return { type: "ACCESS_DENIED" as const };
+
+      return {
+        type: "SUCCESS" as const,
+        activityCollection: await db.activityCollection.delete({
+          where: { id },
+        }),
+      };
+    }),
 });
