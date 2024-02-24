70d9192a5863a0e5cbeebe5c2758e1bae22b63ce
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-24 23:48:32 +0000

    Add custom error message when logs fail

--- src/server/api/utils/log.ts
+++ src/server/api/utils/log.ts
@@ -4,6 +4,10 @@ import { env } from "~/env";
 const axiom = new Axiom({
   token: env.AXIOM_TOKEN,
   orgId: env.AXIOM_ORG_ID,
+  onError: (e) => {
+    console.log("Failed to send log to Axiom, logging error to the console:");
+    console.log(e);
+  },
 });
 
 export const log = (event: {
