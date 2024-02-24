e16bfb5ec4febcbffd72694d96dbe260f5726ab1
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-24 22:52:09 +0000

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+export default function Custom404Page() {
+  return (
+    <main>
+      <div className="flex flex-col items-center pt-24">
+        <h1 className="text-8xl">404</h1>
+        <p>Nothing to see here...</p>
+      </div>
+    </main>
+  );
+}
