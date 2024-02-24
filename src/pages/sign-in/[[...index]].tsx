5ea170c1b06e2357c4f73429aa005ae13b90a9df
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-24 19:56:17 +0000

    Add theme to custom sign in/sign up pages

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,10 +1,18 @@
 import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function SignInPage() {
+  const { resolvedTheme } = useTheme();
+
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
-        <SignIn />
+        <SignIn
+          {...(resolvedTheme === "dark"
+            ? { appearance: { baseTheme: dark } }
+            : {})}
+        />
       </div>
     </main>
   );
--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,10 +1,18 @@
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function SignUpPage() {
+  const { resolvedTheme } = useTheme();
+
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
-        <SignUp />
+        <SignUp
+          {...(resolvedTheme === "dark"
+            ? { appearance: { baseTheme: dark } }
+            : {})}
+        />
       </div>
     </main>
   );
