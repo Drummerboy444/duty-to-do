b26f249cd0e621f67d26a02d6b9fd1d29c5a4420
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-25 22:30:45 +0000

    Add activity collection display cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,50 @@ import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 
-// const ActivityCollectionCard = ({}: {
-//   name: string;
-//   description: string;
-//   createdAt: string;
-// }) => {
+const ActivityCollectionCard = ({
+  activityCollection: { name, description, createdAt },
+}: {
+  activityCollection: {
+    name: string;
+    description: string;
+    createdAt: Date;
+  };
+}) => {
+  return (
+    <div className="flex flex-col rounded-xl border border-gray-300 p-4 dark:border-gray-500">
+      <h2 className="pb-4 text-xl">{name}</h2>
+      <p className="pb-2">{description}</p>
+      <div className="grow" />
+      <div className="flex justify-end">
+        <div className="text-sm text-gray-500">
+          Created {dayjs(createdAt).fromNow()}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// };
+const ActivityCollectionGrid = ({
+  activityCollections,
+}: {
+  activityCollections: {
+    id: string;
+    name: string;
+    description: string;
+    createdAt: Date;
+  }[];
+}) => {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {activityCollections.map((activityCollection) => (
+        <ActivityCollectionCard
+          key={activityCollection.id}
+          activityCollection={activityCollection}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default function HomePage() {
   const {
@@ -40,13 +77,7 @@ export default function HomePage() {
           }}
         />
       </div>
-      {activityCollections.map((activityCollection) => (
-        <div key={activityCollection.id}>
-          <div>Name: {activityCollection.name}</div>
-          <div>Description: {activityCollection.description}</div>
-          <div>Created: {dayjs(activityCollection.createdAt).fromNow()}</div>
-        </div>
-      ))}
+      <ActivityCollectionGrid activityCollections={activityCollections} />
     </main>
   );
 }
