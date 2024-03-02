0a5b7146a37dde28a993c7275c9a9f177f70e163
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-02 12:31:12 +0000

    Test conditional rendering of Clerk sign-in component

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,6 +1,16 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, useUser } from "@clerk/nextjs";
+// import { useRouter } from "next/navigation";
+import { LoadingPage } from "~/components/LoadingPage";
+// import { HOME_ROUTE } from "~/utils/routing";
 
 export default function SignInPage() {
+  const { isLoaded, isSignedIn } = useUser();
+  // const router = useRouter();
+
+  if (!isLoaded) return <LoadingPage />;
+
+  if (isSignedIn) return undefined;
+
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
