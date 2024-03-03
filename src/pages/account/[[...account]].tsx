deea29f021ca3e107a1a740435a200bbc037e0dc
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-03 10:54:59 +0000

    Fix account styling

--- src/pages/account/[[...account]].tsx
+++ src/pages/account/[[...account]].tsx
@@ -3,7 +3,9 @@ import { ACCOUNT_ROUTE } from "~/utils/routing";
 
 export default function AccountPage() {
   return (
-    <main className="flex justify-center py-4 sm:py-8 lg:py-12">
+    // The pb-12 is so that the Clerk badge never goes off of the edge of the
+    // screen as it is positioned at the bottom on small screens.
+    <main className="flex justify-center pb-12 pt-4 sm:pt-8 lg:pt-12">
       <UserProfile routing="path" path={ACCOUNT_ROUTE} />
     </main>
   );
