fc572e29ceea895fe038e8d5e543908c131defa5
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-04 20:20:57 +0000

    Add stats to the admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -4,6 +4,15 @@ import { LoadingPage } from "~/components/LoadingPage";
 import { api } from "~/utils/api";
 import { isAdmin } from "~/utils/is-admin";
 
+const AdminCard = ({ name, value }: { name: string; value: string }) => {
+  return (
+    <div className="flex justify-between gap-2 rounded-xl border border-gray-300 p-4 dark:border-gray-500">
+      <div>{name}:</div>
+      <div>{value}</div>
+    </div>
+  );
+};
+
 export default function AdminPage() {
   const { user, isLoaded: userIsLoaded } = useUser();
 
@@ -25,8 +34,19 @@ export default function AdminPage() {
     return <ErrorPage message="Error fetching statistics" />;
 
   return (
-    <div>
-      This is the admin page. Here are some statistics: {statisticsData}
-    </div>
+    <main className="px-8 py-12 sm:px-16 lg:px-24">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <AdminCard name="Users" value={statisticsData.userCount.toString()} />
+        <AdminCard
+          name="Activity collections"
+          value={statisticsData.activityCollectionCount.toString()}
+        />
+        <AdminCard
+          name="Activities"
+          value={statisticsData.activityCount.toString()}
+        />
+        <AdminCard name="Tags" value={statisticsData.tagCount.toString()} />
+      </div>
+    </main>
   );
 }
--- src/server/api/routers/admin-router.ts
+++ src/server/api/routers/admin-router.ts
@@ -1,7 +1,18 @@
+import { clerkClient } from "@clerk/nextjs";
 import { adminProcedure, createTRPCRouter } from "../trpc";
 
 export const adminRouter = createTRPCRouter({
-  statistics: adminProcedure.query(() => {
-    return "some-random-data";
+  statistics: adminProcedure.query(async ({ ctx: { db } }) => {
+    const activityCollectionCount = await db.activityCollection.count();
+    const activityCount = await db.activity.count();
+    const tagCount = await db.tag.count();
+    const userCount = await clerkClient.users.getCount();
+
+    return {
+      activityCollectionCount,
+      activityCount,
+      tagCount,
+      userCount,
+    };
   }),
 });
