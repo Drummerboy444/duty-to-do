d6178e4a080a9cd6a0f96fb4c7e674a83d17c749
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-04 21:48:38 +0000

    Implement delete account button

--- src/pages/account/[[...index]].tsx
+++ src/pages/account/[[...index]].tsx
@@ -1,9 +1,95 @@
 import { UserProfile } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import toast from "react-hot-toast";
 import { Button } from "~/components/Button";
-import { ACCOUNT_ROUTE } from "~/utils/routing";
+import { Dialog } from "~/components/Dialog";
+import { LoadingSpinner } from "~/components/LoadingSpinner";
+import { absurd } from "~/utils/absurd";
+import { api } from "~/utils/api";
+import { ACCOUNT_ROUTE, SIGN_UP_ROUTE } from "~/utils/routing";
 
 const DeleteAccountButton = () => {
-  return <Button warn label="Delete account" />;
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const { mutate: deleteMe, isLoading: isDeletingMe } =
+    api.users.deleteMe.useMutation({
+      onSuccess: async ({ type }) => {
+        switch (type) {
+          case "SUCCESS": {
+            setOpen(false);
+            await router.push(SIGN_UP_ROUTE);
+            toast.success("Successfully deleted your account");
+            return;
+          }
+
+          case "UNKNOWN_ERROR": {
+            setOpen(false);
+            toast.error(
+              "Something went wrong while deleting your account, please try again later",
+            );
+            return;
+          }
+
+          default: {
+            absurd(type);
+          }
+        }
+      },
+      onError: () => {
+        setOpen(false);
+        toast.error(
+          "Something went wrong while deleting your account, please try again later",
+        );
+      },
+    });
+
+  return (
+    <>
+      <Button
+        warn
+        label="Delete Account"
+        onClick={() => {
+          setOpen(true);
+        }}
+      />
+
+      <Dialog
+        title="Delete Account"
+        open={open}
+        onOpenChange={setOpen}
+        content={
+          <p>
+            Are you sure you want to delete your account? This action will
+            delete all of your activity collections and cannot be undone.
+          </p>
+        }
+        footer={
+          <div className="flex items-center justify-end gap-4">
+            {isDeletingMe && <LoadingSpinner />}
+
+            <Button
+              label="Cancel"
+              onClick={() => {
+                setOpen(false);
+              }}
+              disabled={isDeletingMe}
+            />
+
+            <Button
+              label="Delete"
+              warn
+              onClick={() => {
+                deleteMe();
+              }}
+              disabled={isDeletingMe}
+            />
+          </div>
+        }
+      ></Dialog>
+    </>
+  );
 };
 
 export default function AccountPage() {
