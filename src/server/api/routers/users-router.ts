5e309e9b8a6f6597e09ee69aa074ff4b07530ef4
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-04 21:06:34 +0000

    Add deleteMe mutation

--- src/server/api/routers/users-router.ts
+++ src/server/api/routers/users-router.ts
@@ -1,3 +1,26 @@
-import { createTRPCRouter } from "../trpc";
+import { clerkClient } from "@clerk/nextjs";
+import { createTRPCRouter, privateProcedure } from "../trpc";
+import { SUCCESS } from "../utils/generic-responses";
+import { log } from "../utils/log";
 
-export const usersRouter = createTRPCRouter({});
+export const usersRouter = createTRPCRouter({
+  deleteMe: privateProcedure.mutation(async ({ ctx: { db, userId } }) => {
+    try {
+      await db.$transaction(async (tx) => {
+        await tx.activityCollection.deleteMany({ where: { ownerId: userId } });
+        await clerkClient.users.deleteUser(userId);
+      });
+
+      return SUCCESS;
+    } catch (error) {
+      log({
+        level: "ERROR",
+        message: "Error while deleting user",
+        userId,
+        additionalData: { error },
+      });
+
+      return { type: "UNKNOWN_ERROR" as const };
+    }
+  }),
+});
