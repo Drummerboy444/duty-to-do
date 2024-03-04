4218348a26060a79d4f5fcf04a57030c32263e63
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-04 21:30:52 +0000

    Add placeholder delete account button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,17 +4,19 @@ import { noOp } from "~/utils/no-op";
 export const Button = ({
   label,
   onClick = noOp,
+  warn = false,
   disabled = false,
 }: {
   label: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
+  warn?: boolean;
   disabled?: boolean;
 }) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className="rounded bg-sky-500 px-3 py-1 text-white shadow hover:bg-sky-600 active:bg-sky-700 disabled:cursor-not-allowed disabled:bg-gray-400"
+      className={`rounded ${warn ? "bg-red-500" : "bg-sky-500"} px-3 py-1 text-white shadow ${warn ? "hover:bg-red-600" : "hover:bg-sky-600"} ${warn ? "active:bg-red-700" : "active:bg-sky-700"} disabled:cursor-not-allowed disabled:bg-gray-400`}
     >
       {label}
     </button>
--- src/pages/account/[[...index]].tsx
+++ src/pages/account/[[...index]].tsx
@@ -1,12 +1,18 @@
 import { UserProfile } from "@clerk/nextjs";
+import { Button } from "~/components/Button";
 import { ACCOUNT_ROUTE } from "~/utils/routing";
+
+const DeleteAccountButton = () => {
+  return <Button warn label="Delete account" />;
+};
 
 export default function AccountPage() {
   return (
-    // The pb-12 is so that the Clerk badge never goes off of the edge of the
-    // screen as it is positioned at the bottom on small screens.
-    <main className="flex justify-center pb-12 pt-4 sm:pt-8 lg:pt-12">
+    <main className="flex flex-col items-center gap-4 pb-12 pt-4 sm:pt-8 lg:pt-12">
       <UserProfile routing="path" path={ACCOUNT_ROUTE} />
+      <div className="z-10">
+        <DeleteAccountButton />
+      </div>
     </main>
   );
 }
