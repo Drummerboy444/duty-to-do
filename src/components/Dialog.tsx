fcd78ef946cbdb3a109bc52fcd4bb7200e69a4d6
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-09 15:18:10 +0000

    Fix dialog scrolling

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -24,11 +24,11 @@ export const Dialog = ({
             event.preventDefault();
             event.stopPropagation();
           }}
-          className="fixed left-1/2 top-1/2 z-30 flex w-5/6 max-w-xl -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded-lg bg-white px-12 py-8 shadow-2xl dark:bg-zinc-900"
+          className="fixed left-1/2 top-1/2 z-30 flex max-h-[calc(100%-2rem)] w-5/6 max-w-xl -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded-lg bg-white px-12 py-8 shadow-2xl dark:bg-zinc-900"
         >
           <RadixDialog.Title className="text-2xl">{title}</RadixDialog.Title>
 
-          {content}
+          <div className="overflow-auto px-6">{content}</div>
 
           {footer}
         </RadixDialog.Content>
