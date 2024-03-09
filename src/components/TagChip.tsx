b83045369b6da8453efa60081643c4f2a7f271a3
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-09 16:15:31 +0000

    Filter activities based off of selected tags

--- src/components/TagChip.tsx
+++ src/components/TagChip.tsx
@@ -21,3 +21,25 @@ export const TagChip = ({
     {name}
   </p>
 );
+
+export const SelectableTagChip = ({
+  tag: { id, name },
+  selected,
+  setSelected,
+}: {
+  selected: boolean;
+  setSelected: (selected: boolean) => void;
+  tag: { id: string; name: string };
+}) => (
+  <p
+    style={{
+      ...(selected ? { backgroundColor: getTagColour(id) } : {}),
+    }}
+    className={`cursor-pointer select-none rounded-full border border-transparent px-4 ${selected ? "text-black" : "border-gray-300 dark:border-gray-700"}`}
+    onClick={() => {
+      setSelected(!selected);
+    }}
+  >
+    {name}
+  </p>
+);
--- src/pages/[activityCollectionId]/index.tsx
+++ src/pages/[activityCollectionId]/index.tsx
@@ -1,15 +1,71 @@
 import { Pencil1Icon } from "@radix-ui/react-icons";
 import Link from "next/link";
+import { useState } from "react";
 import { ErrorPage } from "~/components/ErrorPage";
 import { LoadingPage } from "~/components/LoadingPage";
 import { PageHeader } from "~/components/PageHeader";
+import { SelectableTagChip, TagChip } from "~/components/TagChip";
 import { useSafeActivityCollectionQueryParams } from "~/hooks/use-safe-query-params";
 import { absurd } from "~/utils/absurd";
 import { api } from "~/utils/api";
 import { getActivityCollectionEditRoute } from "~/utils/routing";
 
+const ActivityRow = ({
+  name,
+  tags,
+}: {
+  name: string;
+  tags: { id: string; name: string }[];
+}) => (
+  <div className="flex flex-col gap-2 rounded-lg border border-gray-300 p-4 dark:border-gray-500">
+    <p>{name}</p>
+    {tags.length > 0 && (
+      <div className="flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <TagChip key={tag.id} tag={tag} />
+        ))}
+      </div>
+    )}
+  </div>
+);
+
+const TagSelector = ({
+  selectedTagIds,
+  setSelectedTagIds,
+  allTags,
+}: {
+  selectedTagIds: string[];
+  setSelectedTagIds: (selectedTagIds: string[]) => void;
+  allTags: { id: string; name: string }[];
+}) => {
+  if (allTags.length === 0) return undefined;
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      {allTags.map((tag) => (
+        <SelectableTagChip
+          key={tag.id}
+          tag={tag}
+          selected={selectedTagIds.includes(tag.id)}
+          setSelected={(selected) => {
+            setSelectedTagIds(
+              allTags
+                .filter((otherTag) => {
+                  if (otherTag.id === tag.id) return selected;
+                  return selectedTagIds.includes(otherTag.id);
+                })
+                .map(({ id }) => id),
+            );
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
 export default function ActivityCollectionPage() {
   const queryParams = useSafeActivityCollectionQueryParams();
+  const [selectedTagIds, setSelectedTagIds] = useState<string[]>([]);
 
   const {
     data: activityCollectionData,
@@ -47,6 +103,12 @@ export default function ActivityCollectionPage() {
         activityCollection: { id, name, description, activities },
       } = activityCollectionData;
 
+      const filteredActivities = activities.filter((activity) =>
+        selectedTagIds.every((selectedTagId) =>
+          activity.tags.map(({ id }) => id).includes(selectedTagId),
+        ),
+      );
+
       return (
         <main className="flex flex-col gap-4 px-8 py-12 sm:px-16 lg:px-24">
           <div className="flex justify-between gap-2">
@@ -56,13 +118,29 @@ export default function ActivityCollectionPage() {
             </Link>
           </div>
 
-          {activities.length > 0 ? (
-            <div>
-              {activities.map((activity) => (
-                <p key={activity.id}>{activity.name}</p>
+          <TagSelector
+            selectedTagIds={selectedTagIds}
+            setSelectedTagIds={setSelectedTagIds}
+            allTags={activityCollectionData.activityCollection.tags}
+          />
+
+          {filteredActivities.length > 0 ? (
+            <>
+              {filteredActivities.map((filteredActivity) => (
+                <ActivityRow
+                  key={filteredActivity.id}
+                  name={filteredActivity.name}
+                  tags={filteredActivity.tags}
+                />
               ))}
-            </div>
+            </>
           ) : (
+            <p className="italic text-gray-500">
+              No activities found that match these tags
+            </p>
+          )}
+
+          {activities.length === 0 && (
             <p className="italic text-gray-500">
               You don&apos;t have any activities in this collection, try
               creating one!
