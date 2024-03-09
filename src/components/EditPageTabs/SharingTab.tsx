5bdbdb523cc36b63d57f0ebae13c4590e78c3ea9
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-09 21:04:20 +0000

    Split sharing row into its own component

--- src/components/EditPageTabs/SharingTab.tsx
+++ src/components/EditPageTabs/SharingTab.tsx
@@ -1,5 +1,41 @@
 import Image from "next/image";
 
+const SharingRow = ({
+  activityCollectionId,
+  sharedWith: { id, user },
+}: {
+  activityCollectionId: string;
+  sharedWith: {
+    id: string;
+    user:
+      | {
+          id: string;
+          username: string | null;
+          imageUrl: string;
+        }
+      | "UNKNOWN_USER";
+  };
+}) => {
+  return (
+    <div className="flex items-center gap-2 rounded-lg border border-gray-300 p-4 dark:border-gray-500">
+      {user === "UNKNOWN_USER" ? (
+        <p>Unknown user</p>
+      ) : (
+        <>
+          <Image
+            className="rounded-full"
+            src={user.imageUrl}
+            width={32}
+            height={32}
+            alt="User avatar"
+          />
+          <p>{user.username === null ? "Unknown user" : user.username}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
 export const SharingTab = ({
   activityCollectionId,
   sharedWith,
@@ -19,25 +55,11 @@ export const SharingTab = ({
   return (
     <div className="flex flex-col gap-4">
       {sharedWith.map(({ id, user }) => (
-        <div
+        <SharingRow
           key={id}
-          className="flex items-center gap-2 rounded-lg border border-gray-300 p-4 dark:border-gray-500"
-        >
-          {user === "UNKNOWN_USER" ? (
-            <p>Unknown user</p>
-          ) : (
-            <>
-              <Image
-                className="rounded-full"
-                src={user.imageUrl}
-                width={32}
-                height={32}
-                alt="User avatar"
-              />
-              <p>{user.username === null ? "Unknown user" : user.username}</p>
-            </>
-          )}
-        </div>
+          activityCollectionId={activityCollectionId}
+          sharedWith={{ id, user }}
+        />
       ))}
     </div>
   );
