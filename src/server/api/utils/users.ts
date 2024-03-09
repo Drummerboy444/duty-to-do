ca44403170fecf19979562e41c78d5602fed8df9
Anthony Murphy <anthonymurphy252@gmail.com>
2024-03-09 20:39:08 +0000

    Add image URLs to public user data

--- src/server/api/utils/users.ts
+++ src/server/api/utils/users.ts
@@ -1,9 +1,10 @@
 import { clerkClient } from "@clerk/nextjs";
 import { type User } from "@clerk/nextjs/server";
 
-export const extractPublicUserData = ({ id, username }: User) => ({
+export const extractPublicUserData = ({ id, username, imageUrl }: User) => ({
   id,
   username,
+  imageUrl,
 });
 
 export const extractPrivateUserData = ({ id, username }: User) => ({
